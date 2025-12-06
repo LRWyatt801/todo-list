fbc16ea2a8b34e4f36dba494cab41bae127349df
LRWyatt801 <loganwyatt1995@gmail.com>
2025-12-05 19:01:55 -0600

    Add database context

---
+++ dotnet-todo/dotnet-todo/Data/Dotnet-todoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using dotnet_todo.Models;
+
+namespace dotnet_todo.Data
+{
+    public class Dotnet_todoContext : DbContext
+    {
+        public Dotnet_todoContext (DbContextOptions<Dotnet_todoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todos> Todos { get; set; } = default!;
+    }
+}
